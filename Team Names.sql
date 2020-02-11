5a7eb978368b1e1a146ee93264f364ac174abeee
Brian Loc Nguyen <34972651+NguyenBrianL97@users.noreply.github.com>
2020-02-10 23:25:44 -0800

    Creates additional table

---
+++ Team Names.sql
@@ -0,0 +1,34 @@
+CREATE TABLE team_id (
+	Tm varchar(30),
+    id int(2)
+    );
+INSERT INTO team_id (Tm, id) VALUES ("Arizona Diamondbacks",1);
+INSERT INTO team_id (Tm, id) VALUES ("Atlanta Braves",2);
+INSERT INTO team_id (Tm, id) VALUES ("Baltimore Red Sox",3);
+INSERT INTO team_id (Tm, id) VALUES ("Boston Red Sox",4);
+INSERT INTO team_id (Tm, id) VALUES ("Chicago Cubs",5);
+INSERT INTO team_id (Tm, id) VALUES ("Chicago White Sox",6);
+INSERT INTO team_id (Tm, id) VALUES ("Cincinnati Reds",7);
+INSERT INTO team_id (Tm, id) VALUES ("Cleveland Indians",8);
+INSERT INTO team_id (Tm, id) VALUES ("Colorado Rockies",9);
+INSERT INTO team_id (Tm, id) VALUES ("Detroit Tigers",10);
+INSERT INTO team_id (Tm, id) VALUES ("Houston Astros",11);
+INSERT INTO team_id (Tm, id) VALUES ("Kansas City Royals",12);
+INSERT INTO team_id (Tm, id) VALUES ("Los Angeles Angels",13);
+INSERT INTO team_id (Tm, id) VALUES ("Los Angeles Dodgers",14);
+INSERT INTO team_id (Tm, id) VALUES ("Miami Marlins",15);
+INSERT INTO team_id (Tm, id) VALUES ("Milwaukee Brewers",16);
+INSERT INTO team_id (Tm, id) VALUES ("Minnesota Twins",17);
+INSERT INTO team_id (Tm, id) VALUES ("New York Mets",18);
+INSERT INTO team_id (Tm, id) VALUES ("New York Yankees",19);
+INSERT INTO team_id (Tm, id) VALUES ("Oakland Athletics",20);
+INSERT INTO team_id (Tm, id) VALUES ("Philadelphia Phillies",21);
+INSERT INTO team_id (Tm, id) VALUES ("Pittsburgh Pirates",22);
+INSERT INTO team_id (Tm, id) VALUES ("San Diego Padres",23);
+INSERT INTO team_id (Tm, id) VALUES ("Seattle Mariners",24);
+INSERT INTO team_id (Tm, id) VALUES ("San Francisco Giants",25);
+INSERT INTO team_id (Tm, id) VALUES ("St. Louis Cardinals",26);
+INSERT INTO team_id (Tm, id) VALUES ("Tampa Bay Rays",27);
+INSERT INTO team_id (Tm, id) VALUES ("Texas Rangers",28);
+INSERT INTO team_id (Tm, id) VALUES ("Toronto Blue Jays",29);
+INSERT INTO team_id (Tm, id) VALUES ("Washington Nationals",30);
